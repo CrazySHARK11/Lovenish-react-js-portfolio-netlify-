2770de9cb30428cce9ad4a1de182fb47d058f078
Lovenish <heelme1181@gmail.com>
2023-02-21 20:00:03 +0530

    added hamburgermenu and other

--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -1,30 +1,34 @@
 import { useState , useEffect } from "react"
 import { Link } from "react-router-dom"
+import { RiMenuFill , RiCloseFill } from 'react-icons/all';
  
 export default function Header() {
 
     const[stick , setStick] = useState('')
+    const[active , setActive] = useState(false)
 
     useEffect(()=>{
       window.onscroll = () =>{
-        window.scrollY > 10 ? setStick("sticked") : setStick("")
+        window.scrollY > 10 ? setStick("sticked") : setStick("");
+        setActive(false);
       }
     },[])
-
   return (
-    <header className={stick}>
+    <header className={stick && stick}>
         <div className="wrap">
             <nav>
                 <a href="#" className="logo">
-                    Lovenish <span>.</span>
+                    Lovenishaa <span>.</span>
                 </a>
 
-                <div className="menu">
-                    <Link onClick={()=> window.scrollTo(0 , 0)} to="/"> HOME </Link>
-                    <Link onClick={()=> window.scrollTo(0 , 0)} to="/service"> SERVICE </Link>
-                    <Link onClick={()=> window.scrollTo(0 , 0)} to="/skills"> SKILLS</Link>
-                    <Link onClick={()=> window.scrollTo(0 , 0)} to="/contact"> CONTACT </Link>
+                <div className={`menu ${ active ? "active" : ""}`}>
+                    <Link onClick={()=>{ window.scrollTo(0 , 0); setActive(false)}} to="/"> HOME </Link>
+                    <Link onClick={()=>{ window.scrollTo(0 , 0); setActive(false)}} to="/service"> SERVICE </Link>
+                    <Link onClick={()=>{ window.scrollTo(0 , 0); setActive(false)}} to="/skills"> SKILLS</Link>
+                    <Link onClick={()=>{ window.scrollTo(0 , 0); setActive(false)}} to="/contact"> CONTACT </Link>
                 </div>
+
+               { active ?  <RiCloseFill className="menu-btn" size={"2em"} onClick={()=>setActive(!active)} /> : <RiMenuFill className="menu-btn" size={"2em"} onClick={()=>setActive(!active)} /> }
             </nav>
         </div>
     </header>
