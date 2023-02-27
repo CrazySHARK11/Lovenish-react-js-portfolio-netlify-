121c2164298b96dc625b161de3eec78c08e190ed
Lovenish <heelme1181@gmail.com>
2023-02-27 17:45:27 +0530

    fixed form 2

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -17,7 +17,7 @@ export default function Contact() {
            <p className='mail'>  <span className="cont_icon">  <FaEnvelope/> </span> lovenish.biz@gmail.com </p>
            </div>
 
-            <form action="POST" data-netlify="true" >
+            <form name="contact" method="POST" data-netlify="true" >
               <h3>Send me Message !</h3>
 
               <input type="text" name="name" placeholder='Username *' required />
