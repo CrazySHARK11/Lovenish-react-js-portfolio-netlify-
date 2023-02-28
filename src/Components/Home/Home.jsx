c9245f87e04f67dd5ff71a685de643674a440aae
Lovenish <heelme1181@gmail.com>
2023-02-28 23:22:28 +0530

    changed content

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="ban_con">
           <h1>Hi ! I'M <span>LOVENISH</span> </h1>
           <h3 className="descript">I AM A <span>WEB</span> DEVELOPER </h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.  <br /> Eaque, consequuntur ratione itaque, aliquid libero ipsum ducimuctus quidem.</p>
+            <p>Lets create amazing project that gives <br /> life and identity to a business </p>
             <div className="socials">
               <a href="#"><FaInstagram /></a>
               <a href="#"><FaFacebook /></a>
